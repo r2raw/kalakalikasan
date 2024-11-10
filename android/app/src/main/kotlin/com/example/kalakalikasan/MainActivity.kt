222d37d8d181bc059b4a82dc7946c299eb82ef07
r2raw <martejrii.arturo.07172001@gmail.com>
2024-11-10 21:24:25 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/kalakalikasan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kalakalikasan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:kalakalikasan/widgets/login.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Login(),
+    ),
+  ));
+}
